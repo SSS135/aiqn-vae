cc2c89641734acd4376ccf13179d51149e7199c8
Alexander Penkin <sss13594@gmail.com>
2018-11-29 20:11:21 +0500

    fix

--- main.py
+++ main.py
@@ -129,13 +129,13 @@ def loss_function(recon_x, x, mu, logvar):
 
 def l1_quantile_loss(output, target, tau, reduce=True):
     u = target - output
-    loss = (tau - (u.detach() <= 0).float()).mul_(2).mul_(u)
+    loss = (tau - (u.detach() <= 0).float()).mul_(u)
     return loss.mean() if reduce else loss
 
 
-def huber_quantile_loss(output, target, tau, k=0.05, reduce=True):
+def huber_quantile_loss(output, target, tau, k=0.02, reduce=True):
     u = target - output
-    loss = (tau - (u.detach() <= 0).float()).mul_(2 * u.detach().abs().clamp(max=k).div_(k)).mul_(u)
+    loss = (tau - (u.detach() <= 0).float()).mul_(u.detach().abs().clamp(max=k).div_(k)).mul_(u)
     return loss.mean() if reduce else loss
 
 
